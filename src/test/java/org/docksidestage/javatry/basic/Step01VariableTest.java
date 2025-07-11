d3a3e6cc6fd368e404f8da76b8772d28b396f15b
Rio-Rf-biz <r.iwata@bizreach.co.jp>
2025-07-11 16:40:25 +0900

    07-11のコミット

--- src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step01VariableTest.java
@@ -24,7 +24,7 @@ import org.docksidestage.unit.PlainTestCase;
  * Operate exercise as javadoc. If it's question style, write your answer before test execution. <br>
  * (javadocの通りにエクササイズを実施。質問形式の場合はテストを実行する前に考えて答えを書いてみましょう)
  * @author jflute
- * @author your_name_here
+ * @author Rio-Rf-biz
  */
 public class Step01VariableTest extends PlainTestCase {
 
@@ -47,8 +47,9 @@ public class Step01VariableTest extends PlainTestCase {
         String piari = null;
         String dstore = "mai";
         sea = sea + land + piari + ":" + dstore;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => mystic8:mai
     }
+    //型の異なる変数を+演算子で文字列結合していると考えました。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_basic() {
@@ -56,8 +57,9 @@ public class Step01VariableTest extends PlainTestCase {
         String land = "oneman";
         sea = land;
         land = land + "'s dreams";
-        log(sea); // your answer? => 
+        log(sea); // your answer? => oneman
     }
+    // sea = land;でその時点でのlandの値(="oneman")が代入されるので、seaの値は"oneman"になると考えました。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_int() {
@@ -65,8 +67,9 @@ public class Step01VariableTest extends PlainTestCase {
         int land = 415;
         sea = land;
         land++;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 415
     }
+    // sea = land; でその時点でのlandの値(=415)が代入されるので、seaの値は415になると考えました。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_reassigned_BigDecimal() {
@@ -75,8 +78,13 @@ public class Step01VariableTest extends PlainTestCase {
         sea = land;
         sea = land.add(new BigDecimal(1));
         sea.add(new BigDecimal(1));
-        log(sea); // your answer? => 
+        log(sea); // your answer? => 416
     }
+    // sea.add(new BigDecimal(1)); の部分でインクリメントされると考えて417にしていました。
+    // しかし、sea.add(new BigDecimal(1)); では代入しているわけではないので416になると理解しました。
+    // 気になったのでadd()メソッドについて調べたところ、戻り値はBigDecimal型で返すことを確認しました。
+    // そもそもなぜlog(sea)で値が返ってくるか調べました。
+    // log()ではtoString()が戻り値として呼び出されており、BigDecimalクラスのtoString() メソッドは、そのオブジェクトが表す数値を文字列形式で返すため数値のメンバ変数のみがログに出力される。
 
     // ===================================================================================
     //                                                                   Instance Variable
@@ -89,20 +97,29 @@ public class Step01VariableTest extends PlainTestCase {
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_String() {
         String sea = instanceBroadway;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => NULL
     }
+    // NULLになると予想。初期化されていないので。
+    // 正解はnullだった。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_int() {
         int sea = instanceDockside;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => null
     }
+    // 初期化されていないのでnullになると予想。
+    // 正解は0だった。
+    // 調べたところint型の初期値は0であることがわかりました。
+    // 参考：https://qiita.com/kumaGoro_95/items/3420cbb36f4e9edfcee1
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_default_Integer() {
         Integer sea = instanceHangar;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => null
     }
+    // 初期値をまとめた表に記載がなかったのでnullになると予想。
+    // intは型そのもの、nullは許容されていない。
+    // Integer は int 型の値をラップしたオブジェクト。クラスのインスタンス。
 
     /** Same as the previous method question. (前のメソッドの質問と同じ) */
     public void test_variable_instance_variable_via_method() {
@@ -110,8 +127,19 @@ public class Step01VariableTest extends PlainTestCase {
         instanceMagiclamp = "magician";
         helpInstanceVariableViaMethod(instanceMagiclamp);
         String sea = instanceBroadway + "|" + instanceDockside + "|" + instanceHangar + "|" + instanceMagiclamp;
-        log(sea); // your answer? => 
+        log(sea); // your answer? => bigband|1||burn
     }
+    // helpInstanceVariableViaMethod()で値が変わることを踏まえてbigband|1||burnと予想
+    // 正解はbigband|1|null|magicianだった。
+    //
+    // *nullについて*
+    // nullをString型と+演算子で結合すると、nullは文字列の"null"に変換されて結合されます。
+    // これはJavaの+演算子（文字列結合演算子）の特性によるものです。by gemini
+    //
+    // *instanceMagiclampについて*
+    // Copilotに尋ねたらinstanceMagiclampはメソッドの引数として渡されているローカル変数であるためクラスのメンバ変数には影響しない
+    // 納得した。
+
 
     private void helpInstanceVariableViaMethod(String instanceMagiclamp) {
         instanceBroadway = "bigband";
@@ -130,14 +158,17 @@ public class Step01VariableTest extends PlainTestCase {
         String sea = "harbor";
         int land = 415;
         helpMethodArgumentImmutableMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor
     }
+    // landはメソッドでインクリメントされているがローカル変数なので変化なし、seaも同様、以上よりharborと予想
 
     private void helpMethodArgumentImmutableMethodcall(String sea, int land) {
         ++land;
         String landStr = String.valueOf(land); // is "416"
         sea.concat(landStr);
     }
+    // landStrにはint型の416をString型に変換したものが代入される
+    // concat()メソッドはString型のメソッドで、元の文字列に引数で渡した文字列を連結した新しい文字列を返す。
 
     // -----------------------------------------------------
     //                                   Mutable Method-call
@@ -147,8 +178,15 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentMethodcall(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor
     }
+    // helpMethodArgumentMethodcall(sea, land)こちらはローカル変数なのでseaの値は変化しないと予想。
+    // 以上よりharborと予想
+    // 正解はharbor416だった。
+    // 引数でStringBuilder型を渡すと参照渡しになる。
+    // そのため、メソッド内でのseaの変更は呼び出し元のseaにも影響を与えると理解した。
+    // オブジェクト自体は、メモリ上に存在するデータの塊であり、変数はそのオブジェクトがメモリ上のどこにあるかを示す「アドレス（参照）」を保持しています。
+    // ↑らしい。たしかに納得。
 
     private void helpMethodArgumentMethodcall(StringBuilder sea, int land) {
         ++land;
@@ -163,8 +201,9 @@ public class Step01VariableTest extends PlainTestCase {
         StringBuilder sea = new StringBuilder("harbor");
         int land = 415;
         helpMethodArgumentVariable(sea, land);
-        log(sea); // your answer? => 
+        log(sea); // your answer? => harbor
     }
+    // メソッド内ではローカル変数のseaに新しいインスタンスへの参照を渡しているだけなので、harborと予想
 
     private void helpMethodArgumentVariable(StringBuilder sea, int land) {
         ++land;
@@ -193,6 +232,10 @@ public class Step01VariableTest extends PlainTestCase {
      */
     public void test_variable_writing() {
         // define variables here
+        String sea = "mystic";
+        Integer land = null;
+        int piari = 0;
+        log(sea, land, piari);
     }
 
     // ===================================================================================
