77130ba6f54b4027674f0fab184db28205907949
jflute <jflute@localhost>
2026-03-06 18:18:14 +0900

    AIの話のコメント

--- src/test/java/org/docksidestage/javatry/basic/Step08Java8FunctionTest.java
+++ src/test/java/org/docksidestage/javatry/basic/Step08Java8FunctionTest.java
@@ -481,4 +481,6 @@ public class Step08Java8FunctionTest extends PlainTestCase {
     }
 
     // *Stream API will return at Step12 again, it's worth the wait!
+    
+    // #1on1: AIで書いたコードの可読性をいつまでこだわるか？話 (2026/03/06)
 }
